b14268fba303653f58c239f690aa829418356323
Angelo Zovaro <angelo.z@aluno.ifsp.edu.br>
2024-09-05 18:22:16 -0300

    Projeto Trilha Net POO finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456",modelo: "Modelo 1", imei: "1111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Zapzap");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "34123123", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("X");
